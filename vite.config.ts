56d27910703ceb342b0aeae74d193fc6aa9cb751
Kaifur Rahaman <kaifur.rahaman.145@gmail.com>
2026-03-14 23:27:04 +0530

    fix render host

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-        allowedHosts: true
+        allowedHosts: ["vibesnap-5mac.onrender.com"]
       },
       plugins: [
         react(),
