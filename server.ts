0405ce23eeff5e8027b81d0980de0136e2544bb5
Kaifur Rahaman <kaifur.rahaman.145@gmail.com>
2026-03-16 23:59:10 +0530

    fix start script for render

--- server.ts
+++ server.ts
@@ -20,7 +20,7 @@ async function startServer() {
   const PORT = 3000;
 
   // MongoDB Connection
-  const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://kaifurrahaman145_db_user:YOUR_PASSWORD@cluster0.isbqz5q.mongodb.net/myDatabase";
+  const MONGODB_URI = process.env.MONGODB_URI;
   try {
     await mongoose.connect(MONGODB_URI);
     console.log("Connected to MongoDB Atlas successfully");
@@ -28,7 +28,8 @@ async function startServer() {
     console.error("MongoDB connection error:", error);
   }
 
-  app.use(express.json());
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
   app.use(cookieParser());
 
   // Spotify Config
